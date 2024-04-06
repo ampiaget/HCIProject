a4d2b23dc4757d2bfa7051415d7913a5dfe05e90
Mariela Badillo <mlbadillo8@gmail.com>
2024-04-06 15:44:28 -0400

    project setup and navigation
    Created the project and navbar for navigation between two main pages

---
+++ prototype/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Post from './components/Post';
+import Navbar from './components/NavBar';
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/post" element={<Post />} />
+                </Routes>
+            </div>
+        </Router>
+        {/* <p> Welcome to Clever Cook! </p> */}
+        </div>
+        
+    );
+}
+
+export default App;
